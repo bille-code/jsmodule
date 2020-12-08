649d692ea23bb8b30ca7ab6574f0c2ae03ec919a
bille <bille@iteso.mx>
2020-12-08 12:43:03 -0600

    Fist branch commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 console.log('Hola Koders')
+let name_Koder = prompt('¿ Cuál es tu nombre?')
+console.log(name_Koder)
+
+//case style
+//CamelCase
+//PascalCase
+//snañe_case
+let age_koder = prompt('¿ Cuál es tu edad?')
+    console.log(age_koder)
+
+let resulsuma = parseInt(age_koder ) + 5
+    console.log(resulsuma)
+let multi = parseInt (age_koder) * 5
+    console.log(multi)
+let div = parseFloat(age_koder) / 5
+    console.log(div)
+let rest = parseInt(age_koder -5)
+    console.log(rest)
