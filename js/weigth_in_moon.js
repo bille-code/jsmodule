7231d465b91fe9ab9acb659f84c1aab6d7468869
bille <bille@iteso.mx>
2020-12-08 19:50:52 -0600

    Add example weithMoon

--- js/weigth_in_moon.js
+++ js/weigth_in_moon.js
@@ -1,6 +1,7 @@
 let weigth_koder = prompt('¿ Cual es tu peso?' )
 
-let weigth_moon = parseFloat(weigth_koder) / 9.81 * 1.622
+let weigth_moon = parseFloat(weigth_koder / 9.81) * 1.622
 
-alert('tu peso en la luna es:' +  weigth_moon)
-
+let result = ('tu peso en la luna es:' +  weigth_moon  + '  Kg.')
+let resultTwo = `Tu peso en la luna es: ${weigth_moon} Kg`
+alert(resultTwo)
