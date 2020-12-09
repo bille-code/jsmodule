8367324d8316a5c066a3d8a2fa75dcc22f572544
bille <bille@iteso.mx>
2020-12-08 21:40:23 -0600

    ADD switch sentence

--- js/climate_day.js
+++ js/climate_day.js
@@ -1,21 +1,42 @@
-let climate_day = prompt ('¿ Como está el clima el día de hoy')
+const climate_day = prompt ('¿ Como está el clima el día de hoy')
 
-if ('lluvioso'== climate_day) {
+/* if ('lluvioso' === climate_day) {
+         console.log('El día esta lluvioso y hay muchos charcos')
+}
+        else if ('soleado' === climate_day) {
+            console.log ('El día está muy caluroso')
+        }
+        else if ('nevando' === climate_day){
+            console.log ('El día esta para no salir de casa')
+        }
+        else if ('nublado' === climate_day) {
+            console.log ('El día esta para tomar cafe')
+        }
+    else {
+        const other_climate = prompt('Describe el clima en tu localidad ')
+        console.log (other_climate)
+
+
+} */
+
+switch(climate_day) {
+    case 'lluvioso' :
     console.log('El día esta lluvioso y hay muchos charcos')
-}
+    break
+    case 'soleado':
+        console.log ('El día está muy caluroso')
+    break
+    case 'nevado' :
+        console.log ('El día esta para no salir de casa')
+    break
+    case 'nublado' :
+        console.log ('El día esta para tomar cafe')
+    break
+    default:
+        const other_climate = prompt('Describe el clima en tu localidad ')
+        console.log (other_climate)
 
-if('soleado' === climate_day) {
-    console.log ('El día está muy caluroso')
-}
-if ('nevando' === climate_day){
-    console.log ('El día esta para no salir de casa')
-}
-if ('nublado' === climate_day) {
-    console.log ('El día esta para tomar cafe')
-}
-else {
-    let other_climate = prompt('Describe el clima en tu localidad ')
-    console.log (other_climate)
+
 
 
 }
