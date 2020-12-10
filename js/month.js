5bceab09b2d9a936522d3be1624f781084b6eb65
bille <bille@iteso.mx>
2020-12-10 11:31:32 -0600

    ADD month file

--- js/month.js
+++ js/month.js
@@ -5,3 +5,28 @@
 // 5 -> 31 días
 
 const MONTH = prompt('Introduce un numero del 1 al 12')
+
+    switch(MONTH){
+        case 1:
+        case 3:
+        case 5:
+        case 7:        
+        case 8:
+        case 10:
+        case 12:
+            break
+            console.log("Mes tiene 31 días")
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            break
+            console.log("Mes tiene 30 días")
+        case 2:
+            break
+            console.log("Mes tiene 28 0 29 días")
+        default:
+            console.log("Numero invalido debes introducir numero entre 1 y 12")
+    }
+
+        
