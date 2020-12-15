cd487f3bd035847913f29a601dab4653067e03e8
bille <bille@iteso.mx>
2020-12-14 20:16:14 -0600

    ADD changes

---
+++ js/exersice_class.js
@@ -0,0 +1,18 @@
+//Escribir una funcion que a partir del String
+//---> "987"
+//imprimir las tablas de multiplicar en ese orden
+//tabla del 9
+//tabla del 8
+// tabla del 7
+
+function tablas(_stringNumber){
+        const table = _stringNumber
+        const resul = table.charat(table-1)
+        /* for( j=_stringNumber; j<=_stringNumber.lenght-1; j--){
+                for(let i=1; i>=10; i++){
+                        
+                }
+                console.log(`tabla ${_stringNumber} es ${_stringNumber * i}`)
+        } */
+        console.log(table)
+}
