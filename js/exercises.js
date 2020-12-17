ca1c1e3462f87eb23b559511a75e590a2f6e9c8d
bille <bille@iteso.mx>
2020-12-16 19:12:00 -0600

    Add exercise file class-seven

---
+++ js/exercises.js
@@ -0,0 +1,48 @@
+// Ejercicio 1 .forEach()
+// Dado un array con solo números
+// obtener el valor mas alto
+// input: [1,4,3,2,5]
+// output: -> [5]
+
+//Declarar un arreglo de solo numeros
+//Declarar un arreglo vacio para guardar el valor comparado
+// 
+    let count = 0 
+    const highNum = [1,4,3,2,5]
+    highNum.forEach(currentElement => {
+        
+            if(currentElement > count){
+                count = currentElement
+            /* high.push(currentElement) */
+            }         
+            
+        })
+       console.log(count)
+
+
+
+// ejercicio 2 .map()
+// función
+// convertir todos los elementos numericos de un array
+// a strings
+// input: [1,2,3,4,5]
+// output -> ['1,','2','3','4','5']// ejercicio 3 .map()
+// función
+// capitalizar todos los elementos que sean strings de un array
+// input: ['hOlA', 'mundo', 123]
+// output -> ['Hola','Mundo','123]// ejercicio 4 .filter()
+//  funcion
+// dado un array de números, filtre los elementos
+// y devuelva un array con los elementos que no estan duplicados
+// [1,2,4,2,5,4]
+// -> [1,5]// ejercicio 5 .filter()
+//  funcion
+// dado un array de números, filtre los elementos
+// y devuelva un array con los elementos duplicados
+// [1,2,4,2,5,4]
+// -> [2,4]// ejercicio 7 .filter()
+//  funcion
+// dado un array de años, filtre los elementos
+// y devuelva un array con los años que son bisiesto
+// [1990, 2000, 2001, 2020]
+// -> [2000,2020]
