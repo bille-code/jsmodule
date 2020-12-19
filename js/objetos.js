81f80067789c8c9c6821a8a22d028297d3a9fc74
bille <bille@iteso.mx>
2020-12-18 20:18:43 -0600

    Add reduce file

---
+++ js/objetos.js
@@ -0,0 +1,21 @@
+let person {
+    name:'Rubén',
+    location: {
+        cp:'1234'
+        street:'assasd'
+    }
+}
+
+//Mi nombre es Rubén y vivo en la calle XXX deon el CP2544
+
+console.log (`mi nombre es ${person.name} y vivo en la calle ${person.location.street} con el CP ${person.location.cp}`)
+
+
+
+
+
+
+
+
+let person2 = new Object {}
+person2.name
---
+++ js/reduce.js
@@ -0,0 +1,37 @@
+let occurrence = 0
+const searchOcurrence = ['a','b','c','c','b']
+searchOcurrence.reduce((accumulator,currentValue)=>{
+    if (currentValue === occurrence){
+
+    }
+return occurrence
+}, 0)
+
+
+
+//ejercicio clase jueves 17 Dic
+
+let numberReduce = [
+    [1,2,4],
+    [1,2,8],
+    [1,9,3],
+]
+numberReduce.reduce((accumulator,currentValue)=>{
+    let suma = accumulator + currentValue
+    // console.log(accumulator, currentValue);
+    return suma
+}, 0)
+
+
+let koder = {
+
+        name: 'Rubén',
+        lastname: 'Flores',
+        generacion: 'Python',
+        actionOne: 'tomar clase',
+        actionTwo: 'realizar ejercicios',
+        actionthree: 'desertar',
+
+
+
+}
